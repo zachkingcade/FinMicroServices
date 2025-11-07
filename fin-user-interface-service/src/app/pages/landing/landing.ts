bae4368c433476f864158952535425f8c2ba16ca
Zachary Kingcade <zachkingcade@gmail.com>
2025-11-07 06:14:02 -0600

    Fin User Interface Service angular project new

---
+++ fin-user-interface-service/src/app/pages/landing/landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.scss',
+})
+export class Landing {
+
+}
